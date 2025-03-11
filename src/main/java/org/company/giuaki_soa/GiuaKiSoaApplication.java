33fd986362487763b780dc7dd5f0bb2d4e4c4a53
Vanduyn39 <phanthivanduyen@gmail.com>
2025-03-11 13:29:19 +0700

    First commit

---
+++ src/main/java/org/company/giuaki_soa/GiuaKiSoaApplication.java
@@ -0,0 +1,13 @@
+package org.company.giuaki_soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiuaKiSoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiuaKiSoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/company/giuaki_soa/GiuaKiSoaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.company.giuaki_soa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiuaKiSoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
